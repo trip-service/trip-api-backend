189517b163d655076bccd90c3331350d8a3d1818
帕斯昂 <passonTomas@linweiqindeMacBook-Pro.local>
2022-06-16 00:39:52 +0800

    add node env to uat

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: 'trip-backend-api',
+        script: 'server.js',
+        watch: '.',
+        env: {
+          NODE_ENV: 'uat',
+        },
+      },
+    ],
+  };
+  
